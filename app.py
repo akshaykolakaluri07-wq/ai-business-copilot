40d0442b6df3b68e749f966b59c3f404bcce01e4
akshaykolakaluri07-wq <akshaykolakaluri07@gmail.com>
2026-04-13 11:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from db.database import init_db
-from agents.langgraph_orchestrator import handle_query
+from agents.orchestrator import handle_query
 
 st.set_page_config(page_title="AI Business Copilot")
 
@@ -13,15 +13,6 @@ query = st.text_input("Ask a business question:")
 if st.button("Run"):
     if query:
         result = handle_query(query)
-
-        if result is None:
-            st.error("No result returned")
-
-        elif isinstance(result, str):
-            st.write(result)
-
-        else:
-            st.dataframe(result)  # 🔥 force dataframe display
-
+        st.write(result)
     else:
         st.warning("Enter a query")
