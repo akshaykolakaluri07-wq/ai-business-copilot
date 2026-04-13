4e1432fd87b4a9c9968232a9ac5d18a63e577236
akshaykolakaluri07-wq <akshaykolakaluri07@gmail.com>
2026-04-13 11:10:05 +0530

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,41 +1,20 @@
 import sqlite3
 import pandas as pd
-import os
 
-# 🔹 Base directory (important for Streamlit Cloud)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# 🔹 Correct DB path
-DB_PATH = os.path.join(BASE_DIR, "sales.db")
-
-# 🔹 Correct CSV path
-DATA_PATH = os.path.join(BASE_DIR, "..", "data", "sales.csv")
-
+DB_PATH = "sales.db"
 
 def init_db():
-    try:
-        conn = sqlite3.connect(DB_PATH)
-
-        df = pd.read_csv(DATA_PATH)
-
-        df.to_sql("sales", conn, if_exists="replace", index=False)
-
-        conn.close()
-        print("DB initialized successfully")
-
-    except Exception as e:
-        print("DB INIT ERROR:", e)
-
+    conn = sqlite3.connect(DB_PATH)
+    df = pd.read_csv("data/sales.csv")
+    df.to_sql("sales", conn, if_exists="replace", index=False)
+    conn.close()
 
 def run_query(query):
     conn = sqlite3.connect(DB_PATH)
-
     try:
         df = pd.read_sql(query, conn)
         return df
-
     except Exception as e:
-        return f"SQL ERROR: {str(e)}"
-
+        return str(e)
     finally:
         conn.close()
